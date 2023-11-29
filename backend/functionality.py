d7db3c6de56648f0d9f276a6c8a88a345c2672c2
quinns7 <quinns@carleton.edu>
2023-11-29 10:31:30 -0600

    trying to create database

--- backend/functionality.py
+++ backend/functionality.py
@@ -2,37 +2,76 @@ import psycopg2
 import psqlConfig
 import dummydata
 
-conn = None
-try:
-    conn = psycopg2.connect(dbname="", 
-                            user= psqlConfig.username, 
-                            host = "localhost", 
-                            port = 7777, 
-                            password= psqlConfig.password)
-except(Exception, psycopg2.Error) as error:
-            print("Error connecting to PostgreSQL", error) 
+def database_exists(dbname, connection_params):
+    try:
+        with psycopg2.connect(**connection_params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("SELECT 1 FROM pg_database WHERE datname = %s;", (dbname,))
+                return cursor.fetchone() is not None
+    except (Exception, psycopg2.Error) as error:
+        print("Error checking database existence:", error)
+        return False
 
-#Creates a cursor 
-cur = conn.cursor()
+def create_database(dbname, connection_params):
+    try:
+        with psycopg2.connect(**connection_params) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute("CREATE DATABASE {};".format(dbname))
+                print("Database '{}' created successfully.".format(dbname))
+    except (Exception, psycopg2.Error) as error:
+        print("Error creating database:", error)
 
-#grabs a specific column list from the specifed table.  
-#Returns a list of tuples
-def select_query(table, column):
-    query = 'SELECT ' + column + 'from ' + table
-    cur.execute(query)
-    results = cur.fetchall
-    return results
+# def execute_sql_script(script_path, connection_params):
+#     try:
+#         with psycopg2.connect(**connection_params) as conn:
+#             with conn.cursor() as cursor:
+#                 with open(script_path, "r") as script_file:
+#                     sql_script = script_file.read()
+#                     cursor.execute(sql_script)
+#                 print("SQL script executed successfully.")
+#     except (Exception, psycopg2.Error) as error:
+#         print("Error executing SQL script:", error)
 
-#Fetches a row in the chosen table based on a specific column and id
-#Useful if looking for data related to a specifc user, item, etc.
-#Returns a list of tuples
-def select_data(table, column, id):
-    query = 'SELECT * from ' + table + " WHERE " + column + " = " + id
-    cur.execute(query) 
-    result = cur.fetchall
-    return result
+# def connect():
+#     try:
+#         conn = psycopg2.connect(dbname="postgres", 
+#                                 user= psqlConfig.username, 
+#                                 host = "localhost", 
+#                                 port = 7777, 
+#                                 password= psqlConfig.password)
+#     except(Exception, psycopg2.Error) as error:
+#                 print("Error connecting to PostgreSQL", error) 
+#     cur = conn.cursor()
+#     return cur
+
+# #grabs a specific column list from the specifed table.  
+# #Returns a list of tuples
+# def select_query(table, column):
+#     cur = connect()
+#     query = 'SELECT ' + column + 'from ' + table
+#     cur.execute(query)
+#     results = cur.fetchall
+#     return results
+
+# #Fetches a row in the chosen table based on a specific column and id
+# #Useful if looking for data related to a specifc user, item, etc.
+# #Returns a list of tuples
+# def select_data(table, column, id):
+#     cur = connect()
+#     query = 'SELECT * from ' + table + " WHERE " + column + " = " + id
+#     cur.execute(query) 
+#     result = cur.fetchall
+#     return result
 
 if __name__ == "__main__":
-      dummydata.createDataInUsers()
-      dummydata.createDataInListings()
-      print(select_data("listings", "title", "lamp"))
+    conn = { "dbname": "postgres", 
+                "user": psqlConfig.username, 
+                "host" : "localhost", 
+                "port" : 5432, 
+                "password": psqlConfig.password}
+    if not database_exists("carlmart", conn):
+        create_database("carlmart", conn)
+    # execute_sql_script("createtables.sql", conn)
+    # dummydata.createDataInUsers()
+    # dummydata.createDataInListings()
+    # print(select_data("listings", "title", "lamp"))
